8fc05ec313fb9b71d4f034197cc74d6755ce37c7
van-create <ivdmeliseev@edu.hse.ru>
2025-04-06 21:24:18 +0300

    small rename

--- Shapes/src/ru/academits/eliseev/shapes_comparators/ShapeAreaComparator.java
+++ Shapes/src/ru/academits/eliseev/shapes_comparators/ShapeAreaComparator.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 
 public class ShapeAreaComparator implements Comparator<Shape> {
     @Override
-    public int compare(Shape s1, Shape s2) {
-        return Double.compare(s1.getArea(), s2.getArea());
+    public int compare(Shape shape1, Shape shape2) {
+        return Double.compare(shape1.getArea(), shape2.getArea());
     }
 }
 
--- Shapes/src/ru/academits/eliseev/shapes_comparators/ShapePerimeterComparator.java
+++ Shapes/src/ru/academits/eliseev/shapes_comparators/ShapePerimeterComparator.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 
 public class ShapePerimeterComparator implements Comparator<Shape> {
     @Override
-    public int compare(Shape s1, Shape s2) {
-        return Double.compare(s1.getPerimeter(), s2.getPerimeter());
+    public int compare(Shape shape1, Shape shape2) {
+        return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());
     }
 }
 
