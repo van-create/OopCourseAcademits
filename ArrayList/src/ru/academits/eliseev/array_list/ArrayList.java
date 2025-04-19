48f8d502342ad54c544d75ec5b395ee949ca99be
van-create <ivdmeliseev@edu.hse.ru>
2025-04-19 14:59:50 +0300

    add toArray data compatibility check

--- ArrayList/src/ru/academits/eliseev/array_list/ArrayList.java
+++ ArrayList/src/ru/academits/eliseev/array_list/ArrayList.java
@@ -93,6 +93,10 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public <T> T[] toArray(T[] array) {
+        if (!array.getClass().getComponentType().isAssignableFrom(items.getClass().getComponentType())) {
+            throw new ArrayStoreException("Невозможно преобразовать массив в массив заданного типа");
+        }
+
         if (array.length < size) {
             //noinspection unchecked
             return (T[]) Arrays.copyOf(items, size, array.getClass());
